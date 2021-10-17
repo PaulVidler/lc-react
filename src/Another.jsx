aa96f2367857df600c9d9a665cbc10c8b2589b09
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2021-10-17 12:02:54 +1000

    Episode 1

---
+++ src/Another.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Another(props) {
+    return (
+        <div>
+            Another Component for { props.name }
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+import Another from './Another'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1 );
+  }
+  const increment = () => {
+    setCount(prevCount => prevCount + 1 );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <span>{ count }</span>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+        </div>
+        <Another name="Paul" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
