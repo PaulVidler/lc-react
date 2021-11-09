577f6dc2a03c8f4ba273c77f06261d3ba148b9b3
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2021-11-09 11:43:01 +1000

    Splitting components
    WIP

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+
+    const [todoInput, setTodoInput] = useState('');
+
+  function handleInput(event) {
+    setTodoInput(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if(todoInput.trim().length === 0){
+        return;
+    }
+
+    props.addTodo(todoInput);
+
+    setTodoInput('');
+
+  }
+
     return (
-        <form action="#" onSubmit={addTodo}>
+        <form action="#" onSubmit={handleSubmit}>
           <input
             type="text"
             value={todoInput}
